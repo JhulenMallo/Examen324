c37a75f714d1af04c4d5a3a6efe73b3d87d15cab
JhulenMallo <104355815+JhulenMallo@users.noreply.github.com>
2022-04-27 02:48:18 -0400

    Add files via upload

---
+++ Ejercicio8/WebService1.asmx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Ejercicio8
+{
+    /// <summary>
+    /// Descripción breve de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+
+        [WebMethod]
+        public DataSet GET()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "server=(local);database=mibasejhulen;user=examen324;pwd=123456";
+            SqlDataAdapter ada = new SqlDataAdapter();
+            ada.SelectCommand = new SqlCommand();
+            ada.SelectCommand.Connection = con;
+            ada.SelectCommand.CommandText = "select * from persona";
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            ada.Fill(ds);
+            return ds;
+        }
+
+        [WebMethod]
+        public string INSERT(int ci, string nombre, string fecha, string depto)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "server=(local);database=mibasejhulen;user=examen324;pwd=123456";
+            SqlDataAdapter ada = new SqlDataAdapter();
+            ada.SelectCommand = new SqlCommand();
+            ada.SelectCommand.Connection = con;
+            ada.SelectCommand.CommandText = string.Format("insert into persona values ('{0}','{1}','{2}','{3}')", ci, nombre, fecha, depto);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            ada.Fill(ds);
+            return "Insercion Exitosa";
+        }
+
+        [WebMethod]
+        public string UPDATE(int id, int ci, string nombre, string fecha, string depto)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "server=(local);database=mibasejhulen;user=examen324;pwd=123456";
+            SqlDataAdapter ada = new SqlDataAdapter();
+            ada.SelectCommand = new SqlCommand();
+            ada.SelectCommand.Connection = con;
+            ada.SelectCommand.CommandText = string.Format("update persona set ci='{1}', nombre_completo='{2}', fecha_nacimiento='{3}', departamento='{4}' where ci='{0}'", id, ci, nombre, fecha, depto);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            ada.Fill(ds);
+            return "Actualizacion Exitosa";
+        }
+
+        [WebMethod]
+        public string DELETE(int id)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "server=(local);database=mibasejhulen;user=examen324;pwd=123456";
+            SqlDataAdapter ada = new SqlDataAdapter();
+            ada.SelectCommand = new SqlCommand();
+            ada.SelectCommand.Connection = con;
+            ada.SelectCommand.CommandText = string.Format("delete persona where ci='{0}'", id);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            ada.Fill(ds);
+            return "Eliminacion Exitosa";
+        }
+    }
+}
